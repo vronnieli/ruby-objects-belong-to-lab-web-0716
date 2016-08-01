20119868776f9f07b0fe647fcde9fe023b7567a6
vronnieli <vronnieli@gmail.com>
2016-08-01 10:08:28 -0400

    Done.

---
+++ lib/artist.rb
@@ -0,0 +1,9 @@
+class Artist
+
+  attr_reader :name
+
+  def name= (name)
+    @name = name
+  end
+
+end
---
+++ lib/author.rb
@@ -0,0 +1,9 @@
+class Author
+
+  attr_reader :name
+
+  def name= (name)
+    @name = name
+  end
+
+end
